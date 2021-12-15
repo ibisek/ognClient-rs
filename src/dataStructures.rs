09b8d758652ad841b94cf81a19f723077bf5b1de
ibisek <panotrix@gmail.com>
2021-12-15 10:56:49 +0100

    initial project import

---
+++ src/dataStructures.rs
@@ -0,0 +1,170 @@
+#![allow(non_snake_case)]
+
+use std::fmt;
+use std::io::{Result};
+
+// use serde::{Serialize, Deserialize};
+use serde_json;
+use serde_json::json;
+
+// #[derive(Serialize, Deserialize)]
+pub struct AircraftBeacon {
+    ts: u64,
+    prefix: String,
+    addr: String,
+    addr_type: AddressType,
+    lat: f64,
+    lon: f64,
+    altitude: i32,
+    course:u64,
+    speed:u32,
+    climb_rate: f64,
+    turn_rate: f64,
+    stealth: bool,
+    do_not_track: bool, 
+    aircraft_type: AircraftType,
+}
+
+impl AircraftBeacon {
+    pub fn new( ts: u64, prefix: String, addr: String, addr_type: AddressType,
+        lat: f64, lon: f64, altitude: i32,
+        course:u64, speed:u32, climb_rate: f64, turn_rate: f64, 
+        stealth: bool, do_not_track: bool, aircraft_type: AircraftType) -> Result<Self> {
+
+        Ok(Self {ts, prefix, addr, addr_type, lat, lon, altitude, course, speed, climb_rate, turn_rate, stealth, do_not_track, aircraft_type})
+    }
+
+    pub fn to_json_str(&self) -> String {
+        // let serialized = serde_json::to_string(self).unwrap();
+        let js = json!({
+            "ts": self.ts,
+            // "prefix": self.prefix,
+            "addr": self.addr,
+            "addr_type": self.addr_type.value(),
+            "lat": format!("{:.5}", self.lat),
+            "lon": format!("{:.5}", self.lon),
+            "alt": self.altitude,
+            "course":  self.course,
+            "speed": self.speed,
+            "vert_speed": format!("{:.1}", self.climb_rate),
+            "turn_rate": format!("{:.1}", self.turn_rate),
+            "stealth": self.stealth,
+            "dnt": self.do_not_track,
+            "acft_type": self.aircraft_type.value(),
+        });
+        
+        js.to_string()
+    }
+}
+
+pub enum AddressType {
+    Unknown,
+    Icao,
+    Flarm,
+    Ogn,
+}
+
+impl AddressType {
+    pub fn from(value: u8) -> AddressType {
+        match value {
+            1 => AddressType::Icao,
+            2 => AddressType::Flarm,
+            3 => AddressType::Ogn,
+            _ => AddressType::Unknown,
+        }
+    }
+
+    pub fn value(&self) -> u8 {
+        match *self {
+            AddressType::Unknown => 0,
+            AddressType::Icao => 1,
+            AddressType::Flarm => 2,
+            AddressType::Ogn => 3,
+        }
+    }
+
+    pub fn to_string(&self) -> String {
+        match *self {
+            AddressType::Unknown => String::from("Unknown (0)"),
+            AddressType::Icao => String::from("ICA (1)"),
+            AddressType::Flarm => String::from("FLR (2)"),
+            AddressType::Ogn => String::from("OGN (3)"),
+        }
+    }
+}
+
+impl fmt::Display for AddressType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
+}
+
+pub enum AircraftType {
+    Undefined,
+    Glider,
+    TowPlane,
+    Helicopter,
+    Parachute,
+    DropPlane,
+    HangGlider,
+    Paraglider,
+    PistonPlane,
+    JetPlane,
+    Unknown,
+    Baloon,
+    Airship,
+    Uav,
+    Reserved,
+    Obstacle,
+}
+
+impl AircraftType {
+    pub fn from(value: u8) -> AircraftType {
+        match value {
+            0 => AircraftType::Undefined,
+            1 => AircraftType::Glider,
+            2 => AircraftType::TowPlane,
+            3 => AircraftType::Helicopter,
+            4 => AircraftType::Parachute,
+            5 => AircraftType::DropPlane,
+            6 => AircraftType::HangGlider,
+            7 => AircraftType::Paraglider,
+            8 => AircraftType::PistonPlane,
+            9 => AircraftType::JetPlane,
+            10 => AircraftType::Unknown,
+            11 => AircraftType::Baloon,
+            12 => AircraftType::Airship,
+            13 => AircraftType::Uav,
+            14 => AircraftType::Reserved,
+            15 => AircraftType::Obstacle,
+            _ => AircraftType::Unknown,
+        }
+    }
+
+    pub fn value(&self) -> u8 {
+        match *self {
+            AircraftType::Undefined => 0,
+            AircraftType::Glider => 1,
+            AircraftType::TowPlane => 2,
+            AircraftType::Helicopter => 3,
+            AircraftType::Parachute => 4,
+            AircraftType::DropPlane => 5,
+            AircraftType::HangGlider => 6,
+            AircraftType::Paraglider => 7,
+            AircraftType::PistonPlane => 8,
+            AircraftType::JetPlane => 9,
+            AircraftType::Unknown => 10,
+            AircraftType::Baloon => 11,
+            AircraftType::Airship => 12,
+            AircraftType::Uav => 13,
+            AircraftType::Reserved => 14,
+            AircraftType::Obstacle => 15,
+        }
+    }
+}
+
+impl fmt::Display for AircraftType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.value())
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,150 @@
+// user ibisek pass -1 vers ognlogbook 0.1 filter r/49.3678/16.1144/200
+
+use chrono::prelude::*;
+
+use std::io::{Result};
+// use std::String;
+// use std::char;
+use std::process;
+use std::str;
+use std::{thread, time};
+use regex::Regex;
+use lazy_static::lazy_static;
+
+mod configuration;
+use configuration::{AIRCRAFT_REGEX, SERVER_ADDR};
+use configuration::{HANDSHAKE_TEMPLATE, HANSHAKE_FILTER_TEMPLATE};
+
+mod dataStructures;
+use dataStructures::{AircraftBeacon, AddressType, AircraftType};
+
+mod ibiStream;
+use ibiStream::IbiStream;
+
+fn rx_time_to_utc_ts(rx_time: &str) -> u64 {
+    let hour = rx_time[0..2].parse::<u32>().unwrap();
+    let min = rx_time[2..4].parse::<u32>().unwrap();
+    let sec = rx_time[4..].parse::<u32>().unwrap();
+
+    let utc: DateTime<Utc> = Utc::now(); 
+    let utc = utc.with_hour(hour).unwrap().with_minute(min).unwrap().with_second(sec).unwrap().with_nanosecond(0).unwrap();
+    
+    utc.timestamp() as u64
+}
+
+fn parse_beacon_line(line: &str) -> Option<Result<AircraftBeacon>> {
+    lazy_static! {
+        static ref AIRCRAFT_RE: Regex = Regex::new(AIRCRAFT_REGEX).unwrap();    
+    }
+    
+    let caps = match AIRCRAFT_RE.captures(line) {
+        Some(caps) => caps,
+        None => return None,
+    };
+    // println!("CAPS: {:?}", caps);
+
+    let prefix = caps.get(1).unwrap().as_str().to_string();
+    // let addr1 = caps.get(2).unwrap().as_str();
+    let rx_time = caps.get(3).unwrap().as_str();
+    let lat = caps.get(4).unwrap().as_str();
+    let lat_letter = caps.get(5).unwrap().as_str();
+    let lon = caps.get(6).unwrap().as_str();
+    let lon_letter = caps.get(7).unwrap().as_str();
+    // let aprs_symbol = caps.get(8).unwrap().as_str();
+    let course: u64 = caps.get(9).unwrap().as_str().parse().unwrap();
+    let speed: u64 = caps.get(10).unwrap().as_str().parse().unwrap();           // [kt]
+    let altitude: f64 = caps.get(11).unwrap().as_str().parse().unwrap();        // [ft]
+    let flags: u8 = u8::from_str_radix(caps.get(12).unwrap().as_str(), 16).unwrap();
+    let addr2 = caps.get(13).unwrap().as_str().to_string();
+    let vertical_speed: f64 = caps.get(14).unwrap().as_str().parse().unwrap();  // [fpm]
+    let angular_speed: f64 = caps.get(15).unwrap().as_str().parse().unwrap();
+    // let flight_level: f64 = caps.get(16).unwrap().as_str().parse().unwrap();     // [flight level ~ hundrets of ft]
+
+    let ts = rx_time_to_utc_ts(rx_time);   // convert rx_time to UTC ts
+    
+    // convert latitude to number:
+    let signum = if lat_letter == "N" {1.0} else {-1.0};
+    let lat = signum * lat[0..2].parse::<f64>().unwrap() + lat[2..].parse::<f64>().unwrap()/60.0;
+    
+    // convert longitude to number:
+    let signum = if lon_letter == "E" {1.0} else {-1.0};
+    let lon = signum * lon[0..3].parse::<f64>().unwrap() + lon[3..].parse::<f64>().unwrap()/60.0;
+    
+    let speed = (speed as f64 * 1.852).round() as u32;  // [kt] -> [km/h]
+
+    // parse flags & aircraft type  STxxxxaa
+    let stealth: bool = if flags & 0b1000_0000 > 0 {true} else {false};
+    let do_not_track: bool = if flags & 0b0100_0000 > 0 {true} else {false};
+    let aircraft_type: AircraftType = AircraftType::from(flags >> 2 & 0x0F);
+    let address_type: AddressType = AddressType::from(flags & 0b0000_0011);
+
+    let vertical_speed = vertical_speed * 0.00508;    // ft per min -> meters/s
+
+    // convert altitude in FL to meters:
+    let altitude = (altitude * 0.3048).round() as i32;
+    
+    // let re = Regex::new(AIRCRAFT_REGEX_ALT).unwrap();
+    // let flight_level: i32 = match re.captures(line) {
+    //     Some(caps) => {
+    //         let fl: f64 = caps.get(1).unwrap().as_str().parse().unwrap();
+    //         (fl * 100.0 * 0.3048).round() as i32  // [FL]->[m]
+    //     },
+    //     None => 0,
+    // };
+        
+    let beacon = AircraftBeacon::new(ts, prefix, addr2, address_type, lat, lon, altitude, course, speed, vertical_speed, angular_speed, stealth, do_not_track, aircraft_type);
+    Some(beacon)
+}
+
+fn main() -> std::io::Result<()> {
+    // let line = "ICA4B43D0>OGFLR,qAS,Brunnen:/202242h4654.10N/00837.09EX339/122/A=002428 !W45! id0D4B43D0 +238fpm +0.1rot 9.2dB -0.5kHz gps2x3 s7.03 h03 rDDAE09 +6.9dBm";
+    // let line = "OGNC35002>OGNTRK,qAS,Sobesice:/081541h4913.50N/01634.47E'000/000/A=001053 !W81! id07C35002 +000fpm +0.0rot FL010.53 5.8dB 1e +5.2kHz gps4x6";
+    // parse_beacon_line(&line);
+    // process::exit(1);
+
+    // let user = "ibisek";
+    // let lat = 49.1234;
+    // let lon = 16.4567;
+    // let range = 100;
+    // let filter = format!(HANSHAKE_FILTER_TEMPLATE, lat, lon, range);
+    // println!("f: {}", filter);
+    // let h = format!(String::from(HANDSHAKE_TEMPLATE, user, filter));
+    // println!("h: {}", h);
+    // process::exit(1);
+
+    // let stream = TcpStream::connect(SERVER_ADDR).unwrap();
+    // stream.set_nonblocking(true).expect("set_nonblocking call failed");
+    // let mut ibi_stream = IbiStream::new(stream)?;
+    let mut ibi_stream = IbiStream::new(SERVER_ADDR)?;
+    ibi_stream.connect();
+
+    println!("Entering the loop..");
+    let supported_beacons: Vec<&str> = vec!["OGN", "FLR", "ICA"];
+    let mut i = 0;
+    // while i <= 666 {
+    loop {
+        let line: String = match ibi_stream.read() {
+            Some(line) => line,
+            None => continue,
+        };
+
+        if line.len() > 3 {
+            let prefix = &line[0..3];
+            if supported_beacons.contains(&prefix) {  
+                i += 1;
+                // println!("[{:06}] LINE1: {}", i, line); 
+                let beacon: AircraftBeacon = match parse_beacon_line(&line) {
+                    Some(res) => res.unwrap(),
+                    None => continue,
+                };
+
+                println!("beacon: {}", beacon.to_json_str());
+
+            // } else {
+            //     println!("[---] LINE: {}", line); 
+            }
+        }
+    }
+    
+    Ok(())
+} // the stream is closed here
