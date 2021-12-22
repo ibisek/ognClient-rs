66ca0720a68f06ad60a736f10bb4d9310aa8cb55
ibisek <ibisek@protonmail.com>
2021-12-22 19:03:33 +0100

    new default filter

--- src/configuration.rs
+++ src/configuration.rs
@@ -5,6 +5,7 @@ pub const SERVER_ADDR: &str = "aprs.glidernet.org:14580";   // port filtered
 // pub const SERVER_ADDR: &str = "aprs.glidernet.org:10152";   // port unfiltered .. whatever that means
 
 pub const DEFAULT_APRS_FILTER: &str = "r/49.3678/16.1144/999999";
+// pub const DEFAULT_APRS_FILTER: &str = "m/200";
 
 pub const AIRCRAFT_REGEX: &str = "^([A-Z]{3})(.{6}).+?/([0-9]{6})h(.+)([NS]).(.+?)([EW])(.{1})(\\d{3})/(\\d{3})/A=([0-9]+).+?id(.{2})(.{6}).+?([+-].+)fpm.([+-].+)rot";
 // pub const AIRCRAFT_REGEX_FL: &str = "FL([0-9.]+)";
