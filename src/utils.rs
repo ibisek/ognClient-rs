1bdb1f65f17d2977d4ef6d582a8d76bd049aa26b
ibisek <ibisek@protonmail.com>
2022-07-26 11:27:54 +0200

    nicer now() formatting

--- src/utils.rs
+++ src/utils.rs
@@ -1,14 +1,17 @@
 use chrono::{Utc, Datelike, Timelike};
+use chrono::format::strftime;
 
 pub fn now() -> String {
     let now = Utc::now();
-    format!(
-        "{:?}-{:02}-{:02} {:02}:{:02}:{:02}",
-        now.year_ce().1,
-        now.month(),
-        now.day(),
-        now.hour(),
-        now.minute(),
-        now.second(),
-    )
+    // format!(
+    //     "{:?}-{:02}-{:02} {:02}:{:02}:{:02}",
+    //     now.year_ce().1,
+    //     now.month(),
+    //     now.day(),
+    //     now.hour(),
+    //     now.minute(),
+    //     now.second(),
+    // )
+
+    format!("{}", now.format("%Y-%m-%d %H:%M:%S%.3f"))
 }
