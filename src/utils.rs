524d825c66a09080308686d70a2065ea40c88e88
ibisek <ibisek@protonmail.com>
2022-12-19 12:03:27 +0100

    cleanup

--- src/utils.rs
+++ src/utils.rs
@@ -1,17 +1,6 @@
-use chrono::{Utc, Datelike, Timelike};
-use chrono::format::strftime;
+use chrono::Utc;
 
 pub fn now() -> String {
     let now = Utc::now();
-    // format!(
-    //     "{:?}-{:02}-{:02} {:02}:{:02}:{:02}",
-    //     now.year_ce().1,
-    //     now.month(),
-    //     now.day(),
-    //     now.hour(),
-    //     now.minute(),
-    //     now.second(),
-    // )
-
     format!("{}", now.format("%Y-%m-%d %H:%M:%S%.3f"))
 }
