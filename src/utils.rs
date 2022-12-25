15618a4b1ec1a593b11f054b8d1104c9e1c7bbda
ibisek <ibisek@protonmail.com>
2022-12-25 11:40:58 +0100

    +from_caps()

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,17 @@
 use chrono::Utc;
+use regex::Captures;
 
 pub fn now() -> String {
     let now = Utc::now();
     format!("{}", now.format("%Y-%m-%d %H:%M:%S%.3f"))
 }
+
+
+// safely parses out a string from regex Captures
+pub fn from_caps<'a>(caps: &'a Captures<'a>, i: usize, default: &'a str) -> &'a str {
+    match caps.get(i) {
+        Some(m) => m.as_str(),
+        None => default,
+    }
+}
+
